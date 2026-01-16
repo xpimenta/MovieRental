f5fb38261d81006537021598614614e8bde2d019
xpimenta <matheuspr96@hotmail.com>
2026-01-16 01:32:08 +0000

    In the MovieFeatures class, there is a method to list all movies, tell us your opinion about it.

--- MovieRental/Movie/MovieFeatures.cs
+++ MovieRental/Movie/MovieFeatures.cs
@@ -2,27 +2,34 @@
 
 namespace MovieRental.Movie
 {
-	public class MovieFeatures : IMovieFeatures
-	{
-		private readonly MovieRentalDbContext _movieRentalDb;
-		public MovieFeatures(MovieRentalDbContext movieRentalDb)
-		{
-			_movieRentalDb = movieRentalDb;
-		}
+    public class MovieFeatures : IMovieFeatures
+    {
+        private readonly MovieRentalDbContext _movieRentalDb;
+        public MovieFeatures(MovieRentalDbContext movieRentalDb)
+        {
+            _movieRentalDb = movieRentalDb;
+        }
 		
-		public Movie Save(Movie movie)
-		{
-			_movieRentalDb.Movies.Add(movie);
-			_movieRentalDb.SaveChanges();
-			return movie;
-		}
+        public Movie Save(Movie movie)
+        {
+            _movieRentalDb.Movies.Add(movie);
+            _movieRentalDb.SaveChanges();
+            return movie;
+        }
 
-		// TODO: tell us what is wrong in this method? Forget about the async, what other concerns do you have?
-		public List<Movie> GetAll()
-		{
-			return _movieRentalDb.Movies.ToList();
-		}
-
-
-	}
+        // We Don't have a DTO, so sensitive data is exposed.
+        // If you have too many records, it can be dangerous for memory -> Pagination or filters (category, active) are recommended 
+        public List<Movie> GetAll()
+        {
+            return _movieRentalDb.Movies.ToList();
+        }
+		
+        public List<Movie> GetAll(int pageNumber = 1, int pageSize = 10)
+        {
+            return _movieRentalDb.Movies
+                .Skip((pageNumber - 1) * pageSize) // pula os registros das páginas anteriores
+                .Take(pageSize)                     // pega apenas pageSize registros
+                .ToList();
+        }
+    }
 }
